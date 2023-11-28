adba0e037e2a259b28d6632fa236fcd8b7fa164d
Sarabeshwaran <113020922+Sarabeshwaran@users.noreply.github.com>
2023-11-28 08:16:37 +0530

    Update and rename 1.py to 1A.py

--- 1A.py
+++ 1A.py
@@ -2,17 +2,14 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
- 
-#Create the NumPy array for actual and predicted labels.
+
 actual    = np.array(
   ['Dog','Dog','Dog','Not Dog','Dog','Not Dog','Dog','Dog','Not Dog','Not Dog'])
 predicted = np.array(
   ['Dog','Not Dog','Dog','Not Dog','Dog','Dog','Dog','Dog','Not Dog','Not Dog'])
- 
-#compute the confusion matrix.
+
 cm = confusion_matrix(actual,predicted)
- 
-#Plot the confusion matrix.
+
 sns.heatmap(cm,
             annot=True,
             fmt='g',
