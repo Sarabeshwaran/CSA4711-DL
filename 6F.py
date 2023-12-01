e9d091eb1819c6949481ace700dc63753933cda5
Sarabeshwaran <113020922+Sarabeshwaran@users.noreply.github.com>
2023-12-01 18:07:20 +0530

    Create 6F.py

---
+++ 6F.py
@@ -0,0 +1,28 @@
+# Import necessary libraries
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data  # Features
+y = iris.target  # Labels
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize the Random Forest classifier
+random_forest = RandomForestClassifier(n_estimators=100, random_state=42)  # You can adjust parameters as needed
+
+# Train the classifier on the training set
+random_forest.fit(X_train, y_train)
+
+# Make predictions on the testing set
+y_pred = random_forest.predict(X_test)
+
+# Calculate accuracy
+accuracy = metrics.accuracy_score(y_test, y_pred)
+
+# Print the accuracy
+print(f"Accuracy: {accuracy}")
